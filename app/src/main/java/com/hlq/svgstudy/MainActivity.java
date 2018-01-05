a7b2347e5ec151d9c603d554d99491ad1fe9c0f3
HLQ <925954424@qq.com>
2018-01-06 01:02:47 +0800

    找不到兼容办法 fuck

--- app/src/main/java/com/hlq/svgstudy/MainActivity.java
+++ app/src/main/java/com/hlq/svgstudy/MainActivity.java
@@ -5,7 +5,9 @@ import android.graphics.drawable.Drawable;
 import android.graphics.drawable.VectorDrawable;
 import android.os.Build;
 import android.os.Bundle;
+import android.support.graphics.drawable.AnimatedVectorDrawableCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
@@ -30,6 +32,13 @@ public class MainActivity extends AppCompatActivity {
         } else {
             BitmapDrawable bitmapDrawable = (BitmapDrawable) drawable;
             imageView.setImageDrawable(bitmapDrawable);
+        }
+    }
+
+    public void getAnim(View view) {
+        ImageView imageView = (ImageView) view;
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP){
+            ((AnimatedVectorDrawableCompat) imageView.getDrawable()).start();
         }
     }
 
